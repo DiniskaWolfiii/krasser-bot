01441aac57b20cd881647bd324e0b93c52ba0668
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2021-09-13 00:16:07 +0200

    Update pull.js

--- commands/pull.js
+++ commands/pull.js
@@ -16,7 +16,7 @@ module.exports = {
 		if (interaction.user.id !== ownerId) return await interaction.reply({content: 'Du bist nicht Wolfiii!', ephemeral: true});
         await interaction.reply({content: ':bulb: Pulling...', ephemeral: true})
         shelljs.cd('/home/wolfiii/bots/krasser-bot')
-        shelljs.exec('git pull origin master')
+        shelljs.exec('git pull')
         shelljs.exec('npm i')
         await interaction.editReply(':bulb: Pulled!')
 	},
