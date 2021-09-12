95ec740d388aca8e48d5c80b35ec9d0181ea9f35
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2021-09-13 00:43:48 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,5 +6,6 @@ module.exports = {
  */
     execute (client) {
         console.log(`Succesfully logged in as ${client.user.username} with ID ${client.user.id}!`);
+        client.user.setActivity('mit krassen Wölfen🐺', { type: 'PLAYING' });
     }
 }
