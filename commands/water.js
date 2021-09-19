cd2945bf3f04fb859136b12ec4a90c019daac00d
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2021-09-19 20:02:23 +0200

    Update water.js

--- commands/water.js
+++ commands/water.js
@@ -18,7 +18,7 @@ module.exports = {
         let antworten;
 
         if (waterUser) {
-            if (waterUser.user.id === interaction.user.id) return await interaction.reply({ content: 'Du kannst den Command nicht auf dich selber wirken! Für dich selbst, gib keinen User an.', epehemral: true })
+            if (waterUser.user.id === interaction.user.id) return await interaction.reply({ content: 'Du kannst den Command nicht auf dich selber wirken! Für dich selbst, gib keinen User an.', ephemeral: true })
             antworten = [
                 `*${interaction.user} gibt ${waterUser} Wasser*`,
                 `*${interaction.user} gibt ${waterUser} Wasser*`,
@@ -28,6 +28,9 @@ module.exports = {
                 `*${interaction.user} gibt ${waterUser} Wasser. Stay hydrated!*`,
                 `*${interaction.user} gibt ${waterUser} Wasser. Stay hydrated!*`,
                 `*${interaction.user} gibt ${waterUser} so viel Wasser dass ${waterUser} ertrinkt... Stay hydrated!*`
+                `*Beim Versuch ${waterUser} Wasser zu geben, stolpert ${interaction.user} und übergießt das ganze Wasser auf ${waterUser}...*`,
+                `*Beim Versuch ${waterUser} Wasser zu geben, stolpert ${interaction.user} und übergießt das ganze Wasser auf ${waterUser}... Hoffentlich deckt ${interaction.user}'s sowas ab :grimacing:'`,
+                `*Auf dem Weg ${waterUser} Wasser zu geben, verspührt ${interaction.user} selber Durst und trinkt das ganze Wasser leer...`
             ]
         } else {
             antworten = [
@@ -35,7 +38,12 @@ module.exports = {
                 `*${interaction.user} trinkt Wasser`,
                 `*${interaction.user} trinkt Wasser`,
                 `*${interaction.user} trinkt Wasser`,
-                `*${interaction.user} trinkt so viel Wasser dass ${interaction.user} ertrinkt... Stay hydrated!*`
+                `*${interaction.user} trinkt Wasser`,
+                `*${interaction.user} trinkt Wasser`,
+                `*${interaction.user} trinkt Wasser`,
+                `*${interaction.user} trinkt so viel Wasser dass ${interaction.user} ertrinkt... Stay hydrated!*`,
+                `*Beim Versuch Wasser zu trinken stolpert ${interaction.user} und überkippt was Wasser über die ganzen Klamotten...*`,
+                `*Beim Versuch Wasser zu trinken stolpert ${interaction.user} und ist nun Nass... Hoffentlich ist ${interaction.user} nicht Wasserscheu :grimacing:*`
             ]
         }
         await interaction.reply(antworten[Math.floor(Math.random() * antworten.length)])
