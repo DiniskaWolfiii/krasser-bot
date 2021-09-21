b38071e9d7e52aeaee0c90430052ed2ff130c7b1
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2021-09-21 22:40:45 +0200

    Update kaba.js

--- commands/kaba.js
+++ commands/kaba.js
@@ -20,17 +20,17 @@ module.exports = {
         if (kabaUser) {
             if (kabaUser.user.id === interaction.user.id) return await interaction.reply({ content: 'Du kannst den Command nicht auf dich selber wirken! Für dich selbst, gib keinen User an.', ephemeral: true })
             antworten = [
-                `*${messageUser} gibt ${taggedUser} eine Heisse Schokolade*`,
-                `*${messageUser} gibt ${taggedUser} einen White Hot Chocolate*`,
-                `*${messageUser} gibt ${taggedUser} einen Hazelnut Chocolate*`,
-                `*${messageUser} gibt ${taggedUser} einen Caramel Chocolate*`
+                `*${interaction.user} gibt ${taggedUser} eine Heisse Schokolade*`,
+                `*${interaction.user} gibt ${taggedUser} einen White Hot Chocolate*`,
+                `*${interaction.user} gibt ${taggedUser} einen Hazelnut Chocolate*`,
+                `*${interaction.user} gibt ${taggedUser} einen Caramel Chocolate*`
             ]
         } else {
             antworten = [
-                `*${messageUser} trinkt eine Heisse Schokolade*`,
-                `*${messageUser} trinkt einen White Hot Chocolate*`,
-                `*${messageUser} trinkt einen Hazelnut Chocolate*`,
-                `*${messageUser} trinkt einen Caramel Chocolate*`
+                `*${interaction.user} trinkt eine Heisse Schokolade*`,
+                `*${interaction.user} trinkt einen White Hot Chocolate*`,
+                `*${interaction.user} trinkt einen Hazelnut Chocolate*`,
+                `*${interaction.user} trinkt einen Caramel Chocolate*`
             ]
         }
         await interaction.reply(antworten[Math.floor(Math.random() * antworten.length)])
