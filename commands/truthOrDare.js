1fe53427f58fa603c3ec7f4989814cc9a168e533
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2021-11-16 08:51:55 +0100

    Update truthOrDare.js

--- commands/truthOrDare.js
+++ commands/truthOrDare.js
@@ -29,7 +29,17 @@ module.exports = {
                 'Wer ist dein geheimer Crush?',
                 'Wann hattest du das letzte Mal Sex?',
                 'Hast du schonmal eine Straftat begangen?',
-                'Was würdest du tun wenn du für einen Monat das andere Geschlecht wärst?'
+                'Was würdest du tun wenn du für einen Monat das andere Geschlecht wärst?',
+                'Hast du schonmal Drogen genommen (außer Alkohol und Tabak)?',
+                'Wer aus der Runde sollte am dringensten zum Friseur?',
+                'Was wissen deine Eltern nicht über dich?',
+                'Was war deine Modesünde als Kind?',
+                'Was ist der peinlichste Gegenstand in deinem Kleiderschrank?',
+                'Wie oft wechselst du deine Bettwäsche?',
+                'Warst du schon einmal nackt in der Öffentlichkeit?',
+                'Hast du schonmal jemand geghostet?',
+                'Was war der größte Fehler den du je begangen hast?',
+                'Wen in der Runde würdest du küssen wenn du müsstest?'
             ]
             await interaction.reply('Truth: ' + antworten[Math.floor(Math.random() * antworten.length)])
         }
@@ -42,7 +52,12 @@ module.exports = {
                 'Mach ein Foto von deiner Momentanen Sicht und poste es hier',
                 'Überzeuge den gesamten Chat dazu dass du das andere Geschlecht bist',
                 'Der Chat darf entscheiden was du trinken musst',
-                'Der Chat darf entscheiden was deine Aufgabe ist'
+                'Der Chat darf entscheiden was deine Aufgabe ist',
+                'Benimm dich 2 Minuten wie ein Huhn',
+                'Schreibe eine versaute Nachricht an deinen letzten Discord/WhatsApp Kontakt. Der Chat entscheidet welche Plattform',
+                'Schicke ein Screenshot von der letzten Privatnachricht die du versendet hast',
+                'Sprich mit einem bayrischen/sächsischen/anderem Dialekt',
+                'Führe eine 3-Minütige Stand-Up-Comendyshow auf'
             ]
             await interaction.reply('Dare: ' + antworten[Math.floor(Math.random() * antworten.length)])
         }
