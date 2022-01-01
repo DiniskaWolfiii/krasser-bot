08d1583c605f0822284c1cbf03bf72f7fa859807
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2022-01-01 13:52:33 +0100

    did stuff

--- push-commands.js
+++ push-commands.js
@@ -12,7 +12,7 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(publicToken); // !!! TOKEN ÄNDERN BEIM PUSHEN !!!
+const rest = new REST({ version: '9' }).setToken(token); // !!! TOKEN ÄNDERN BEIM PUSHEN !!!
 
 (async () => {
 	try {
